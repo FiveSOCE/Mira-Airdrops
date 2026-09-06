09c31ec49083264f9300431e096dc293face0afd
FiveS <sgmmsionide@gmail.com>
2026-09-06 21:26:20 +1000

    Resolve MiraFactions API from Bukkit services

--- src/main/java/com/mira/airdrops/hook/MiraFactionsWarzoneBridge.java
+++ src/main/java/com/mira/airdrops/hook/MiraFactionsWarzoneBridge.java
@@ -1,15 +1,15 @@
 package com.mira.airdrops.hook;
 
 import com.mira.airdrops.WarzoneResolver;
-import com.mira.core.api.MiraCore;
 import com.mira.factions.api.MiraFactionsApi;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
 public final class MiraFactionsWarzoneBridge implements WarzoneResolver {
     private final MiraFactionsApi factions;
 
-    public MiraFactionsWarzoneBridge(MiraCore core) {
-        this.factions = core.services().get(MiraFactionsApi.class).orElse(null);
+    public MiraFactionsWarzoneBridge() {
+        this.factions = Bukkit.getServicesManager().load(MiraFactionsApi.class);
     }
 
     @Override
